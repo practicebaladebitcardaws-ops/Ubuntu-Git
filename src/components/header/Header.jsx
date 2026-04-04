9299e243aba85e96b500a319a6cebbee4b0436fd
practicebaladebitcardaws-ops <practicebaladebitcardaws@gmail.com>
2026-04-04 14:50:15 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Venkaiah Kuncham</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Learning DevOps</h5>
         <CTA />
         <HeaderSocial/>
         <div className="me">
